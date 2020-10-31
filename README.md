ea763867c0bd0489d5bf3737fff7699bc4fa4cd3
Shahid N. Shah <shahid@shah.org>
2020-10-31 14:24:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Template Literal based TypeScript Text Templates
+
+This library provides a convenient way to create a single layout file with some partials that can then be replaced using either regular expressions, simple strings, or [template literal](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals) variables/placeholders (regular JavaScript styles).
+
+## Template Sources
+
+A template source is a regular text file that includes at least one "body placeholder" with an optional number of arbitrary "partials". Below is a sample template with a body and a head (see [mod_test-00.tmpl.html](mod_test-00.tmpl.html)). Templates can have as many partials but only one body may be present. The default body replacement variable is `<!-- BODY CONTENT GOES HERE -->` because that's a common HTML convention but the replacement variable may be any arbitrary text.
+
+```html
+<html>
+
+<head>
+    <!-- HEAD PARTIAL CONTENT -->
+</head>
+
+<body>
+    <!-- BODY CONTENT GOES HERE -->
+</body>
+
+</html>
+```
+
+## Generating text from template sources without partials
+
+Assuming the content in [mod_test-00.tmpl.html](mod_test-00.tmpl.html), this is how you would generate text:
+
+```typescript
+  const template = mod.governedTemplate("my-template.html");
+  const content = "variable";
+  const generated = template`This is my body content, which can contain a ${content} or anything else that can go into a TypeScript template literal.`;
+```
+
+Note that once you create the [template literal](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals) called `template` you can use it to generate as many different variations of the file attached to that template literal as you'd like. 
+
+After executing the code above, `generated` will contain:
+
+```html
+<html>
+
+<head>
+    <!-- HEAD PARTIAL CONTENT -->
+</head>
+
+<body>
+    This is my body content, which can contain a variable or anything else that can go into a TypeScript template literal.
+</body>
+
+</html>
+```
+
+## Generating text from template sources with partials
+
+Assuming the content in [mod_test-00.tmpl.html](mod_test-00.tmpl.html), this is how you would generate text with reusable partials:
+
+```typescript
+  const template = mod.governedTemplate(
+    testFilePath("mod_test-00.tmpl.html"),
+    mod.defaultGovernedTemplateOptions({
+      bodyPlaceholderText: "<!-- BODY CONTENT GOES HERE -->",
+      partials: [{
+        placeholder: "<!-- HEAD PARTIAL CONTENT -->",
+        content: "<title>Page Title</title>",
+      }],
+    }),
+  );
+
+  const content = "variable";
+  const generated = template`This is my body content, which can contain a ${content} or anything else that can go into a TypeScript template literal.`;
+```
+
+Note that once you create the [template literal](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals) called `template` you can use it to generate as many different variations of the file attached to that template literal as you'd like. 
+
+After executing the code above, `generated` will contain:
+
+```html
+<html>
+
+<head>
+    <title>Page Title</title>
+</head>
+
+<body>
+    This is my body content, which can contain a variable or anything else that can go into a TypeScript template literal.
+</body>
+
+</html>
+```
---
+++ mod_test.ts
@@ -0,0 +1,102 @@
+import { path, testingAsserts as ta } from "./deps-test.ts";
+import * as mod from "./mod.ts";
+
+function testFilePath(relTestFileName: string): string {
+  return path.join(
+    path.relative(
+      Deno.cwd(),
+      path.dirname(import.meta.url).substr("file://".length),
+    ),
+    relTestFileName,
+  );
+}
+
+Deno.test(`HTML escaped string literal`, async () => {
+  const escaped = mod.htmlEscape`This should be <escaped>`;
+  ta.assertEquals(escaped, "This should be &lt;escaped&gt;");
+});
+
+Deno.test(`Non-HTML escaped string literal with HTML escaped placeholders`, async () => {
+  const escaped = "<be escaped>";
+  const phe = mod.htmlEscapePlaceholders
+    `This should be <not be escaped> ${escaped}`;
+  ta.assertEquals(phe, "This should be <not be escaped> &lt;be escaped&gt;");
+});
+
+Deno.test(`HTML tag raw`, async () => {
+  const div = mod.htmlTag("div");
+  const phe = div`This should be in a div with <no escapes>`;
+  ta.assertEquals(phe, "<div>This should be in a div with <no escapes></div>");
+});
+
+Deno.test(`HTML tag with results escaped`, async () => {
+  const div = mod.htmlTag("div", true);
+  const phe = div`This should be in a div with <escaped tag>`;
+  ta.assertEquals(
+    phe,
+    "<div>This should be in a div with &lt;escaped tag&gt;</div>",
+  );
+});
+
+Deno.test(`HTML template 00`, async () => {
+  const template = mod.governedTemplate(
+    testFilePath("mod_test-00.tmpl.html"),
+    mod.defaultGovernedTemplateOptions({
+      bodyPlaceholderText: "<!-- BODY CONTENT GOES HERE -->",
+      partials: [{
+        placeholder: "<!-- HEAD PARTIAL CONTENT -->",
+        content: "<title>Page Title</title>",
+      }],
+    }),
+  );
+
+  const content = "variable";
+  const generated = template
+    `This is my body content, which can contain a ${content} or anything else that can go into a TypeScript template literal.`;
+
+  const golden = Deno.readTextFileSync(
+    testFilePath("mod_test-00.html.golden"),
+  );
+  ta.assertStrictEquals(generated, golden);
+});
+
+Deno.test(`HTML template 01`, async () => {
+  const template = mod.governedTemplate(
+    testFilePath("mod_test-01.tmpl.html"),
+    mod.defaultGovernedTemplateOptions({
+      bodyPlaceholderText: "<!-- BODY CONTENT GOES HERE -->",
+      partials: [{
+        placeholder: "<!-- SIGNATURE PARTIAL CONTENT GOES HERE -->",
+        content: "Regards,<br> Medigy Team",
+      }],
+    }),
+  );
+  const generated = template`<p>Hi</p>
+
+                                                        Click below to create your new password.
+
+                                                        </p>
+                                                        <p>
+                                                        <table style="width:200px;height:40px;">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td
+                                                                        style="background-color:#1a4e7a;height:30px;width:475px;text-align: center;vertical-align: middle;border-radius: 5px;">
+                                                                        <a href="{{url}}"
+                                                                            style="color:#FFF;height:30px;width:200px;text-decoration:none !important; font-size:15px;text-decoration:none;">Create
+                                                                            Password</a>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                        </p>
+                                                        <p>
+                                                            If you find any difficulty in clicking the above link,
+                                                            please copy and paste the below url into your browser.
+                                                        </p>
+                                                        <p>{{url}}</p>`;
+  const golden = Deno.readTextFileSync(
+    testFilePath("mod_test-01.html.golden"),
+  );
+  ta.assertStrictEquals(generated, golden);
+});
